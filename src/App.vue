8f8cdabfb8daeab034e3ab663bf2bad755e4066c
vanecordero <ing.soft.cordero@gmail.com>
2022-07-26 21:17:08 -0400

    :fire: [REMOVE]: remove unused files

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,7 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-
-    <div class="wrapper">
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/marvel">Marvel</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
-
-<style scoped>
-header{
-    position: fixed;
-    z-index: 1;
-    margin: auto;
-    width: 100%;
-    background: white;
-    top: 0;
-    height: 50px;
-}
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin: 1rem 0;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-
-
-
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: () => import("../views/AboutView.vue"),
-    },
-    {
-      path: "/marvel",
       name: "marvel",
       component: () => import("../views/MarvelView.vue"),
     },
     {
-      path: "/marvel/:id",
+      path: "/character/:id",
       name: "character",
       component: () => import("../views/MarvelCharacterDetailsView.vue"),
     },
