6a9e6f6b297ff7577aa49d2d1d4a4f85b8fae3ca
vanecordero <ing.soft.cordero@gmail.com>
2022-07-26 23:23:35 -0400

    :see_no_evil: [UPDATE](.gitignore): remove api key files

---
+++ src/marvelKey.js
@@ -0,0 +1,7 @@
+//ing soft
+// export const secret_key = "963c0ee68c81adebb9776ff5800b4ce4b96be698";
+// export const public_key = "becf2326adb671025adeba9de436085c";
+
+//cordero prof
+export const secret_key = "c4addbbd90de80c19b167f3f0b77a02e2da8058e";
+export const public_key = "d7c343637ce752814173a38c190e97e3";
